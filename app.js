a9bd0d8352267f03f564d2571bfa99b98cf78708
bernatixer <bernat.torres.bellido@gmail.com>
2016-10-08 01:16:43 +0200

    Minor changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Founddd');
   err.status = 404;
   next(err);
 });
